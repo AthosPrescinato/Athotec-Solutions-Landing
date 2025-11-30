5160d9e62e43dc241129a8261efe3a88ccad8385
Athos Prescinato <athosprescinato@gmail.com>
2025-11-30 11:00:24 -0300

    Change package.json to deploy in gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Athotec-Solutions-Landing/",
   plugins: [react()],
-})
+  base: "/Athotec-Solutions-Landing/",
+});
